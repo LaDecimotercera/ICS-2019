737a5055f138619c540b8143cbeb1fa1a7878448
tracer-ics2019 <tracer@njuics.org>
2019-09-15 00:57:39 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:57:39 up  8:23,  1 user,  load average: 0.35, 0.37, 0.40 fb435697a75bfa0ba0797dfeb011f445356c742e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -62,11 +62,9 @@ static int cmd_info (char *args) {
 
 static int cmd_x_N_EXPR(char *args) {// PA1.1: simplified version
   unsigned int addr, byte, bytes_length;
-//  char *len=strtok(NULL, " ");bytes_length=atoi(len);
-//  char *add=strtok(NULL, " ");addr=atoi(add);
   sscanf(args, "%d 0x%x", &bytes_length, &addr);
   for (byte = 0 ;byte < bytes_length ;byte ++) {
-	printf("0x%08x\n ", addr + byte*16 );
+	 printf("\n0x%08x ", addr + byte*16 );
   }
   return 0;
 }	
