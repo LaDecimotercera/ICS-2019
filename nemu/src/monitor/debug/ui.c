cbf15664d3690c0ffd0393d729b424f97141088b
tracer-ics2019 <tracer@njuics.org>
2019-09-15 00:52:06 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:52:06 up  8:17,  1 user,  load average: 0.72, 0.45, 0.41 fc6f61c94863338d878243951ac3374c9504fab0

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -81,7 +81,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si N", "Single-step execution of N commands", cmd_si_N },
+  { "si", "Single-step execution of N commands", cmd_si_N },
   { "info", "Print program status by option: r(registers); w(watchpoints)", cmd_info },
   { "x", "Scan memory", cmd_x_N_EXPR },
 //  { "p EXPR", "Find the value of the expression EXPR", cmd_p_EXPR },
