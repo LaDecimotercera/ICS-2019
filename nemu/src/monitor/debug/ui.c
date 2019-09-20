0459a4dafbe6866ad7b82ee3d4d88d2fc13270b1
tracer-ics2019 <tracer@njuics.org>
2019-09-20 22:10:07 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:10:07 up  1:53,  1 user,  load average: 0.11, 0.14, 0.19 5cfb29ac7d5df8ccff38f6bec4655909e1119d4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -41,7 +41,10 @@ static int cmd_si_N(char *args) {//single-step execution of N operations
   char *arg = strtok(NULL, " ");
   if (arg) steps = atoi(arg);
   if (steps<=0) cpu_exec(-1);
-  else cpu_exec(steps);
+  else {
+	 for (int step = 0; step < steps; step ++)
+	  cpu_exec(1);
+  }
   return 0;
 }
 
