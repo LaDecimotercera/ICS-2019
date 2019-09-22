12f94e11cb90a0a58028b020ca53f01b5ba344ef
tracer-ics2019 <tracer@njuics.org>
2019-09-22 17:40:25 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:40:25 up 1 day, 41 min,  1 user,  load average: 0.13, 0.10, 0.15 9ec935c6f8257113d65e2a783bc01f61435d2219

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,6 +1,6 @@
-//#include <stdio.h>
-//#include <assert.h>
-//#include "monitor/monitor.c"
+#include <stdio.h>
+#include <assert.h>
+#include "monitor/monitor.c"
 
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
@@ -8,11 +8,11 @@ void ui_mainloop(int);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-  //FILE *fp = fopen("/home/liweikang/ics2019/nemu/tools/gen-expr/input","r");
-  //assert(fp != NULL);
-  //char *str;
-  //while (fgets(str,65536,fp)!=NULL)
-	//  puts(str);
+  FILE *fp = fopen("/home/liweikang/ics2019/nemu/tools/gen-expr/input","r");
+  assert(fp != NULL);
+  char str[65536];
+  while (fgets(str,65536,fp)!=NULL)
+    puts(str);
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
