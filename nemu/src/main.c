ec2b07e6566424697fabec6f506d729c2bc248d3
tracer-ics2019 <tracer@njuics.org>
2019-09-22 23:42:09 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:42:09 up 1 day,  1:33,  1 user,  load average: 0.09, 0.13, 0.09 d95e8a5ab1fd95f3c3f4394a75ea88497f070512

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
-#include "monitor/monitor.c"
+#include "monitor/expr.h"
 
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
@@ -10,9 +11,18 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
   FILE *fp = fopen("/home/liweikang/ics2019/nemu/tools/gen-expr/input","r");
   assert(fp != NULL);
-  char str[65536];
-  while (fgets(str,65536,fp)!=NULL)
-    puts(str);
+  char str[65536]="";
+  while (fgets(str,65536,fp)!=NULL) {
+    int res=atoi(strtok(str," "));
+//    char buf[65536]="";
+    bool True = true;
+	if(res!=expr(strtok(NULL," "),&True)) {
+		printf("Test fault\n");
+		break;
+	}
+	memset(str,'\0',sizeof(str));
+  }
+	
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
