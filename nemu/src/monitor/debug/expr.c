6edad289df75bd6893d23152a4f65655d46f744b
tracer-ics2019 <tracer@njuics.org>
2019-09-22 16:42:50 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:42:50 up 23:43,  1 user,  load average: 0.12, 0.16, 0.18 57f2ecf74a78527b8b0fe2148326029e7faa8030

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -57,11 +57,10 @@ void init_regex() {
 
 typedef struct token {
   int type;
-  char str[1000];
- // char str[32];
+  char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[1000] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
