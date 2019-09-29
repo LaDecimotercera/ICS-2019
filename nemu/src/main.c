aaaa12b3deb3e5d36947e2deaf9f9016a1673251
tracer-ics2019 <tracer@njuics.org>
2019-09-29 14:38:33 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:38:33 up  1:57,  1 user,  load average: 0.08, 0.11, 0.09 d6751bb222e0f1c47fd050e4d55e1cad8a9959d

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[]) {
   int is_batch_mode = init_monitor(argc, argv);
   FILE *fp = fopen("/home/liweikang/ics2019/nemu/tools/gen-expr/input","r");
   assert(fp != NULL);
-  char str[65536]="";
-  while (fgets(str,65536,fp)!=NULL) {
+  char str[65536];
+  while (fgets(str,sizeof(str),fp)) {
     int res=atoi(strtok(str," ")); //printf("%d",res);
 //	sscanf(res, "%d", &strtok(str," "));
 //    char buf[65536]="";
     bool success = true;
 //  printf(strtok(NULL," "));
 //	printf("%d",expr(strtok(NULL," "),&success));
-	char *e = strtok(NULL,"@"); 
+	char *e = strtok(NULL,"\n"); 
 	if(res!=expr(e,&success)) {
 		printf("Test fault\n");
 		break;
