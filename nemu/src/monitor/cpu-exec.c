4a16079104c7853151b9796575ae60c424741e66
tracer-ics2019 <tracer@njuics.org>
2019-09-29 10:53:01 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:53:01 up 30 min,  1 user,  load average: 0.01, 0.13, 0.20 ecb511cf83709e5489855afab1951703ba89cf2e

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -59,7 +59,7 @@ void cpu_exec(uint64_t n) {
   }
 
     /* TODO: check watchpoints here. */
-  if (check_watchpoint()==true) 
+  if (check_watchpoint()) 
 	  nemu_state.state = NEMU_STOP;
 
 #endif
