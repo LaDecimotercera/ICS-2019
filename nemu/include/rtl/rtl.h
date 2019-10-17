b2dfc81f69ff7b6b919802f22f541a391060a289
tracer-ics2019 <tracer@njuics.org>
2019-10-17 19:11:46 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:11:46 up  8:35,  1 user,  load average: 0.24, 0.19, 0.18 92150233bfbc3923f0f446fbadde9279c945b9

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -131,8 +131,7 @@ void interpret_rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 /* RTL pseudo instructions */
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
-  // dest <- ~src1
-  TODO();
+  *dest = ~*src1;
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
@@ -147,8 +146,7 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
-  // dest <- src1[width * 8 - 1]
-  TODO();
+  *dest = src1[width * 8 - 1];
 }
 
 static inline void rtl_mux(rtlreg_t* dest, const rtlreg_t* cond, const rtlreg_t* src1, const rtlreg_t* src2) {
--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -6,3 +6,9 @@ make_EHelper(operand_size);
 
 make_EHelper(inv);
 make_EHelper(nemu_trap);
+make_EHelper(call);
+make_EHelper(push);
+make_EHelper(sub);
+make_EHelper(xor);
+make_EHelper(ret);
+
