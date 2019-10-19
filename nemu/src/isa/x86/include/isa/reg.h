0c879b0fe4b7d1a5cca0b63f16d584f4d72340b1
tracer-ics2019 <tracer@njuics.org>
2019-10-19 17:12:53 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:12:53 up 18:13,  1 user,  load average: 0.63, 0.35, 0.34 e2607593ff347767577ce717354e8a4eaee962a9

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -17,20 +17,20 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
- union {	
-	union {
-			uint32_t _32;
-		    uint16_t _16;
-			uint8_t _8[2];
-	} gpr[8];
+  union {	
+	 union {
+			 uint32_t _32;
+		     uint16_t _16;
+		 	 uint8_t _8[2];
+	 } gpr[8];
 
   /* Do NOT change the order of the GPRs' definitions. */
-	struct {
-             rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-             // vaddr_t pc;
-	};
- };
- vaddr_t pc;
+	 struct {
+              rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+              vaddr_t pc;
+	 };
+  };
+ // vaddr_t pc;
 
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
