d1798f3d1532740990cebe2f83a19b2ef8f5d8dd
tracer-ics2019 <tracer@njuics.org>
2019-10-25 02:40:42 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  02:40:42 up  7:42,  1 user,  load average: 0.17, 0.25, 0.27 bfb213bbf7b8bd702ec120b2712c42a120e4e18e

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,14 +14,29 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_O: rtl_get_OF(dest); break;
+    case CC_B: rtl_get_CF(dest); break;
+    case CC_E: rtl_get_ZF(dest); break;
+    case CC_BE: {
+      rtl_get_CF(&s0); 
+      rtl_get_ZF(&s1);
+      rtl_or(dest, &s0, &s1);
+    } break;
+    case CC_S: rtl_get_SF(dest); break;
+    case CC_L: {
+      rtl_get_SF(&s0);
+      rtl_get_OF(&s1);
+      rtl_xor(dest, &s0, &s1);
+    } break;
+    case CC_LE: {
+      rtl_get_ZF(&s0);
+      rtl_get_SF(&s1);
+      rtl_get_OF(&t0);
+      rtl_xor(&t1, &s1, &t0);
+      //rtl_and(dest, &s0, &t1); WRONG in manual
+      rtl_or(dest, &s0, &t1);
+    }
+      //TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
