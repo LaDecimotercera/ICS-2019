dd78e73cd31a7ef60e8b410d475198e2175f88ee
tracer-ics2019 <tracer@njuics.org>
2019-10-26 02:04:55 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  02:04:55 up 17:16,  1 user,  load average: 1.78, 1.43, 1.16 d497651b8ad4c6d020edf11579b5f051dc05915

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -7,7 +7,9 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  char *tmp = dst;
+  while ((*dst++ = *src++) != '\0');
+  return tmp;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -15,11 +17,21 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *tmp = dst;
+  while (*dst)
+    dst++;
+  while ((*dst++ = *src++) != '\0');
+  return tmp;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while(*s1 == *s2)
+  {
+    if(*s1 == '\0') return 0;
+    s1++;
+    s2++;
+  }
+  return *s1 - *s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
@@ -27,7 +39,11 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  char *xs = (char *)v;
+  while(n--){
+    *xs++ = c;
+  }
+  return v; 
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
@@ -35,7 +51,12 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+  const  char *su1, *su2;
+  int res = 0;
+  for( su1 = s1, su2 = s2; 0 < n; ++su1, ++su2, n--)
+    if ((res = *su1 - *su2) != 0)
+      break;  
+  return res;
 }
 
 #endif
