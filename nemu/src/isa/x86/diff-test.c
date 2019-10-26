067344b0ce26f6ccbc692f5e42752a138f847b85
181220031-Li Weikang <liwk@smail.nju.edu.cn>
2019-10-27 01:58:53 +0800

    before diff-test

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,12 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  if(ref_r->eax != pc.eax || ref_r->ecx != pc.ecx || ref_r->edx != pc.edx ||
+		 ref_r->ebx != pc.ebx || ref_r->esp != pc.esp || ref_r->ebp != pc.ebp ||
+		 ref_r->esi != pc.esi || ref_r->edi != pc.edi || ref_r->eip != pc.eip)
+	//if(cpu.eip != 0x100005)
+    return false;
+  return true;
 }
 
 void isa_difftest_attach(void) {
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -40,7 +40,7 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 
 void* memset(void* v,int c,size_t n) {
   char *xs = (char *)v;
-  while(n--){
+  while(n--) {
     *xs++ = c;
   }
   return v; 
