15ab79238038da621170a89d92c537cdf5a0bfbb
181220031-Li Weikang <liwk@smail.nju.edu.cn>
2019-10-31 11:16:57 +0800

    completed string.c

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,7 +3,10 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t len = 0;
+  while(*s++)
+    len++;
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
@@ -13,7 +16,14 @@ char *strcpy(char* dst,const char* src) {
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char *tmp = dst;
+  while(n){
+  if(!(*dst++ = *src++)) break; 
+    n--; 
+  }
+  while(n--)
+    *dst++ = '\0';  
+  return tmp; 
 }
 
 char* strcat(char* dst, const char* src) {
@@ -35,7 +45,16 @@ int strcmp(const char* s1, const char* s2) {
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  while(n && *s1 && *s2){
+    if(*s1 != *s2)
+      return (unsigned char)*s1 - (unsigned char)*s2;
+    s1++;
+    s2++;
+    n--;
+  }
+  if(!n) return 0;  
+  if(*s1) return 1;  
+  return -1;     
 }
 
 void* memset(void* v,int c,size_t n) {
@@ -47,7 +66,11 @@ void* memset(void* v,int c,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *tmp = out;
+  const char *s = in;
+  while (n--)
+        *tmp++ = *s++;
+    return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
