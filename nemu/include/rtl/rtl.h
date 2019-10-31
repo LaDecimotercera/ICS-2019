068c68dee308171d6a7e3b40c2f0d5e30746536b
tracer-ics2019 <tracer@njuics.org>
2019-10-31 11:36:50 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  11:36:50 up 23 min,  1 user,  load average: 0.46, 0.40, 0.37 2fa78c6f21390b3299f33be68a2fd99c8346c350

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -138,8 +138,8 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   // TODO();
   int32_t tmp = (int32_t)(*src1);
-  tmp = tmp << 32 - 8 * width;
-  tmp = tmp >> 32 - 8 * width;
+  tmp = tmp << (32 - 8 * width);
+  tmp = tmp >> (32 - 8 * width);
   *dest = tmp;  
 }
 
--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,11 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc){
-  return false;
+  if(ref_r->pc!=pc || ref_r->eax!=cpu.eax || ref_r->ecx!=cpu.ecx || \
+  ref_r->edx!=cpu.edx || ref_r->ebx!=cpu.ebx || ref_r->esp!=cpu.esp \
+  || ref_r->ebp!=cpu.ebp || ref_r->esi!=cpu.esi || ref_r->edi!=cpu.edi)
+    return false;
+  return true;
 }
 
 void isa_difftest_attach(void) {
