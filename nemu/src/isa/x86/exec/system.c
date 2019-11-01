298e8d5da91f8e1fd2b187f01811579e71eb3119
181220031-Li Weikang <liwk@smail.nju.edu.cn>
2019-11-01 15:09:49 +0800

    I think the source code is wrong in sbb&adc

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -42,13 +42,13 @@ void pio_write_w(ioaddr_t, uint32_t);
 void pio_write_b(ioaddr_t, uint32_t);
 
 make_EHelper(in) {
-  TODO();
+  //TODO();
 
   print_asm_template2(in);
 }
 
 make_EHelper(out) {
-  TODO();
+  //TODO();
 
   print_asm_template2(out);
 }
