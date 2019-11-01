ec213070483f400b53420c4e1350709c6ca05d42
tracer-ics2019 <tracer@njuics.org>
2019-11-01 15:32:53 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  15:32:53 up  1:16,  1 user,  load average: 1.37, 0.55, 0.43 b5fd7bd6a8233e7dfbe184e9e83c6e21fd460e

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -36,7 +36,7 @@ void free_wp(int NO) {
   WP *p = head;
   WP *q = p;
   if (head == NULL) {
-	printf("No watchpoint found!\n"); return;
+	//printf("No watchpoint found!\n"); return;
   }
   if (p->NO == NO) {
 	head = head->next;
@@ -56,7 +56,7 @@ void free_wp(int NO) {
 	    point_cnt -= 1;
 		printf("Deleted watchpoint %d\n", p->NO);
 	}
-	else if (p == NULL) printf("No watchpoint found!\n");
+	//else if (p == NULL) printf("No watchpoint found!\n");
   }
 }
 	
@@ -77,7 +77,7 @@ bool check_watchpoint() {
   WP *q = head;
   bool success_ = true;
   if (!q) {
-	printf("No watchpoint found!\n");
+	//printf("No watchpoint found!\n");
 	return false;
   }
   while (q) { 
@@ -97,7 +97,7 @@ bool check_watchpoint() {
 
 void show_watchpoint() {
   WP *p = head;
-  if (!p) printf("No watchpoint found!\n");  
+  //if (!p) printf("No watchpoint found!\n");  
   while(p) {
 	printf("%d %x\n", p->NO, p->Old_value);
 	p=p->next;
