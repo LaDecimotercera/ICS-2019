b9a59f3622134f3c74e60bfb6afda5b3a7033b34
tracer-ics2019 <tracer@njuics.org>
2019-11-03 00:44:47 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  00:44:47 up 14:15,  1 user,  load average: 1.88, 1.43, 0.98 c6a1798cdb96cdc9a30a9e1ccb04df18a99a45b8

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -6,7 +6,7 @@
 #define KEYDOWN_MASK 0x8000
 #define I8042_DATA_PORT 0x60
 
-unsigned long long kbs;
+//unsigned long long kbs;
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {//modified
@@ -14,13 +14,16 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
     // 从中读出_DEV_INPUT_KBD_t结构体, 
     // keydown = 1为按下按键, keydown = 0为释放按键. keycode为按键的断码, 没有按键时, keycode为_KEY_NONE.
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbs = inl(I8042_DATA_PORT);
-      kbd->keycode = kbs;
+      kbd->keycode = _KEY_NONE;
+      if (inb(0x64)&0x1)
+        kbd->keycode=inl(I8042_DATA_PORT);
+      kbd->keydown = 0;
+      //kbs = inl(I8042_DATA_PORT);
+      //kbd->keycode = kbs;
       //printf("???: %d\n",kbd->keycode);
-      if (kbd->keycode != _KEY_NONE)
-        kbd->keydown = !(kbd->keydown);
-      //kbd->keydown = 0;
-      //kbd->keycode = _KEY_NONE;
+      //if (kbd->keycode != _KEY_NONE)
+      //  kbd->keydown = !(kbd->keydown);
+      //kbd->keydown=0;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
