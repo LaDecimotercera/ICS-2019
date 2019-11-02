49867d601b2b24488c0b8560615d7c73b66f45a6
tracer-ics2019 <tracer@njuics.org>
2019-11-02 16:15:58 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  16:15:58 up  7:43,  1 user,  load average: 0.46, 0.64, 0.71 20c7b9b5e38fbeb95fc37aebff2b2344ad65cccd

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -5,15 +5,15 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc){
   if(ref_r->eax!=cpu.eax || ref_r->ecx!=cpu.ecx || ref_r->edx!=cpu.edx || 
   ref_r->ebx!=cpu.ebx || ref_r->esp!=cpu.esp || ref_r->ebp!=cpu.ebp || 
   ref_r->esi!=cpu.esi || ref_r->edi!=cpu.edi || ref_r->pc!=cpu.pc) {
-    printf("qemus eax:0x%08x, cpu eax:0x%08x @pc:0x%08x\n", ref_r->eax, cpu.eax, pc);
-    printf("qemus ecx:0x%08x, cpu ecx:0x%08x @pc:0x%08x\n", ref_r->ecx, cpu.ecx, pc);
-    printf("qemus edx:0x%08x, cpu edx:0x%08x @pc:0x%08x\n", ref_r->edx, cpu.edx, pc);
-    printf("qemus ebx:0x%08x, cpu ebx:0x%08x @pc:0x%08x\n", ref_r->ebx, cpu.ebx, pc);
-    printf("qemus esp:0x%08x, cpu esp:0x%08x @pc:0x%08x\n", ref_r->esp, cpu.esp, pc);
-    printf("qemus ebp:0x%08x, cpu ebp:0x%08x @pc:0x%08x\n", ref_r->ebp, cpu.ebp, pc);
-    printf("qemus esi:0x%08x, cpu esi:0x%08x @pc:0x%08x\n", ref_r->esi, cpu.esi, pc);
-    printf("qemus edi:0x%08x, cpu edi:0x%08x @pc:0x%08x\n", ref_r->edi, cpu.edi, pc);
-    printf("qemus pc:0x%08x, cpu pc:0x%08x @pc:0x%08x\n", ref_r->pc, cpu.pc, pc);
+    printf("qemus eax:0x%08x, cpu eax:0x%08x, pc:0x%08x\n", ref_r->eax, cpu.eax, pc);
+    printf("qemus ecx:0x%08x, cpu ecx:0x%08x, pc:0x%08x\n", ref_r->ecx, cpu.ecx, pc);
+    printf("qemus edx:0x%08x, cpu edx:0x%08x, pc:0x%08x\n", ref_r->edx, cpu.edx, pc);
+    printf("qemus ebx:0x%08x, cpu ebx:0x%08x, pc:0x%08x\n", ref_r->ebx, cpu.ebx, pc);
+    printf("qemus esp:0x%08x, cpu esp:0x%08x, pc:0x%08x\n", ref_r->esp, cpu.esp, pc);
+    printf("qemus ebp:0x%08x, cpu ebp:0x%08x, pc:0x%08x\n", ref_r->ebp, cpu.ebp, pc);
+    printf("qemus esi:0x%08x, cpu esi:0x%08x, pc:0x%08x\n", ref_r->esi, cpu.esi, pc);
+    printf("qemus edi:0x%08x, cpu edi:0x%08x, pc:0x%08x\n", ref_r->edi, cpu.edi, pc);
+    printf("qemus pc:0x%08x, cpu pc:0x%08x, pc:0x%08x\n", ref_r->pc, cpu.pc, pc);
     return false;
   }
   return true;
