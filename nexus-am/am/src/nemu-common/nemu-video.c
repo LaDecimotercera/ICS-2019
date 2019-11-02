93d289e8a24a39d50d69366f0a76db39f54e0884
tracer-ics2019 <tracer@njuics.org>
2019-11-03 03:49:44 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  03:49:44 up 17:20,  1 user,  load average: 0.38, 0.59, 0.56 e81874d239eb1756c6561be9c6a4f12692bf00ab

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -24,14 +24,11 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-      uint32_t *pixels = ctl->pixels;
-      int cp_bytes = sizeof(uint32_t) * min(w, 400 - x);
+      
       uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-      for (int j = 0; j < h && y + j < 300; j ++) {
-        memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
-        pixels += w;
-      }
+      for (int row = 0; row < ctl->h; ++row)
+        memcpy(&fb[(row + ctl->y) * screen_width() + ctl->x], &ctl->pixels[row * ctl->w], ctl->w * 4);
+
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
