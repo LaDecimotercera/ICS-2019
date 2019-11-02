9a52b0c8a6b7117467b6c5922e4de462cfeda769
tracer-ics2019 <tracer@njuics.org>
2019-11-03 00:53:05 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  00:53:05 up 14:24,  1 user,  load average: 0.44, 0.75, 0.86 45afbe848134e010542da5eeef5a4a68fe3b72eb

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -10,16 +10,13 @@ unsigned long long kbs;
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {//modified
-    // _DEVREG_INPUT_KBD, AM键盘控制器.
-    // 从中读出_DEV_INPUT_KBD_t结构体, 
-    // keydown = 1为按下按键, keydown = 0为释放按键. keycode为按键的断码, 没有按键时, keycode为_KEY_NONE.
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
       kbs = inl(I8042_DATA_PORT);
       kbd->keycode = kbs;
       //printf("???: %d\n",kbd->keycode);
-      if (kbd->keycode != _KEY_NONE)
-        kbd->keydown = !(kbd->keydown);
-      //kbd->keydown = 0;
+      //if (kbd->keycode != _KEY_NONE)
+      //  kbd->keydown = !(kbd->keydown);
+      kbd->keydown = 0;
       //kbd->keycode = _KEY_NONE;
       return sizeof(_DEV_INPUT_KBD_t);
     }
