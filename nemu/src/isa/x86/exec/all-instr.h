c7d732b51a187d5ae24cbc4cbcf950832e14c6f9
181220031-Li Weikang <liwk@smail.nju.edu.cn>
2019-11-03 23:06:43 +0800

    ALL completed

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -42,7 +42,7 @@ make_EHelper(mul);
 make_EHelper(cltd);
 make_EHelper(cwtl);
 make_EHelper(div);
-make_EHelper(idiv);
+make_EHelper(idiv);//2.2
 make_EHelper(out);
 make_EHelper(in);
 make_EHelper(lidt);
