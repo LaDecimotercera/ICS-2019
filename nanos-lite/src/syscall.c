1062514f74f64f899427bbcf6c82324e4870a048
tracer-ics2019 <tracer@njuics.org>
2019-11-23 15:03:58 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2 (2019-08-28) x86_64 GNU/Linux  15:03:58 up  2:55,  1 user,  load average: 0.62, 0.41, 0.36 b510c2824b00dbf8914add329c8be22edc475464

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,6 +4,9 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
     default: panic("Unhandled syscall ID = %d", a[0]);
