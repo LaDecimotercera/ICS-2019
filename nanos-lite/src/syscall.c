01baa07bfd4a1178f8fc2094c95b4a0b995d1c0f
tracer-ics2019 <tracer@njuics.org>
2019-11-29 15:55:43 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  15:55:43 up  4:18,  1 user,  load average: 0.60, 0.56, 0.56 7417548b1509cd21663e7da47c6dfc44c463972e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -36,7 +36,7 @@ _Context* do_syscall(_Context *c) {
     case SYS_open: c->GPRx = fs_open((const char *)a[1],a[2],a[3]); break;
     case SYS_read: c->GPRx = fs_read(a[1], (void *)a[2], a[3]); break;
     case SYS_close: c->GPRx = fs_close(a[1]); break;
-    case SYS_lseek: c->GPRx=fs_lseek(a[1],a[2],a[3]); break;
+    case SYS_lseek: c->GPRx = fs_lseek(a[1],a[2],a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
