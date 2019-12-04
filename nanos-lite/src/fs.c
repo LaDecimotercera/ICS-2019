1cec70efa108fc2aa141564f1b950516ca04f713
tracer-ics2019 <tracer@njuics.org>
2019-12-05 02:57:19 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  02:57:19 up  1:14,  1 user,  load average: 0.47, 0.47, 0.40 b747a2141bca124c16c38be0fcbcb2d356cb3232

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,9 +41,9 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stderr", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
   {"/dev/events", 0, 0, 0, events_read, invalid_write},
-  {"/dev/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
   {"/dev/fbsync", 0, 0, 0, invalid_read, fbsync_write},
-  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
+  {"/dev/fb", 0, 0, 0, invalid_read, fb_write}, 
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
