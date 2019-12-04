3aae3950e481fda5f3d606df1761d3dfb9b94351
tracer-ics2019 <tracer@njuics.org>
2019-12-05 01:03:48 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  01:03:48 up  3:02,  1 user,  load average: 1.19, 1.62, 0.98 57eb844e843bdb132dc568df11fac88014059a68

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,15 +41,16 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stderr", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
   {"/dev/events", 0, 0, 0, events_read, invalid_write},
+  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
   {"/dev/fbsync", 0, 0, 0, invalid_read, fbsync_write},
   {"/dev/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
-  {"/dev/fb", 0, 0, 0, invalid_read, fb_write},//must placed at last
+  //must placed at last
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[NR_FILES - 1].size = screen_height() * screen_width() * sizeof(uint32_t);
+  file_table[NR_FILES - 3].size = screen_height() * screen_width() * sizeof(uint32_t);
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
