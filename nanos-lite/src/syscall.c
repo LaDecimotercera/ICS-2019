bee63da37868a0b79b9d1b7db7aba28ff91fa52a
tracer-ics2019 <tracer@njuics.org>
2019-12-06 02:50:26 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  02:50:26 up  2:00,  1 user,  load average: 0.22, 0.17, 0.24 c5c2b8b227fef7e4cc9a94e2ba9039f07b6a483b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,12 +2,15 @@
 #include "syscall.h"
 #include <sys/types.h>
 #include <unistd.h>
+#include "proc.h"
+#include "fs.h"
 
 extern int fs_open(const char *pathname, int flags, int mode);
 extern ssize_t fs_read(int fd, void *buf, size_t count);
 extern ssize_t fs_write(int fd, const void *buf, size_t count);
 extern int fs_close(int fd);
 extern off_t fs_lseek(int fd, off_t offset, int whence);
+extern void naive_uload(PCB *pcb, const char *filename);
 
 /*static inline size_t sys_write(int fd, void *buf, size_t count){
   if (fd == 1 || fd == 2){
@@ -30,13 +33,14 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
     case SYS_yield: _yield(); c->GPRx = 0; break;
-    case SYS_exit: _halt(a[1]); break;
+    case SYS_exit: naive_uload(NULL, "/bin/init"); break;
     case SYS_write: c->GPRx = fs_write(a[1], (void *)a[2], a[3]); break; 
     case SYS_brk: c->GPRx = 0; break;
     case SYS_open: c->GPRx = fs_open((const char *)a[1],a[2],a[3]); break;
     case SYS_read: c->GPRx = fs_read(a[1], (void *)a[2], a[3]); break;
     case SYS_close: c->GPRx = fs_close(a[1]); break;
     case SYS_lseek: c->GPRx = fs_lseek(a[1],a[2],a[3]); break;
+    case SYS_execve: naive_uload(NULL, (const char*)a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
