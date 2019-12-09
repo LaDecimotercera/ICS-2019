fe98b123c5cc6029104f154327d80f137ca6a1c7
tracer-ics2019 <tracer@njuics.org>
2019-12-10 05:39:09 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  05:39:09 up  3:24,  1 user,  load average: 1.05, 0.92, 1.00 dbc5f2db5b7c1b4c6f1d42e1fcdeea6ff3a37eb

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -9,7 +9,7 @@ PCB *current = NULL;
 extern void naive_uload(PCB *pcb, const char *filename);
 extern void context_kload(PCB *pcb, void *entry);
 extern void context_uload(PCB *pcb, const char *filename);
-
+extern _Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack, void *entry, void *args);
 void switch_boot_pcb() {
   current = &pcb_boot;
 }
@@ -17,7 +17,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite for the %dth time!", j);
+    Log("Hello World fsrom Nanos-lite for the %dth time!", j);
     j ++;
     _yield();
   }
