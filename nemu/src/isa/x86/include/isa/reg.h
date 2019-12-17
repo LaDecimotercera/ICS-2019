5663467a07938f19349f3a965c64cb6281cea511
tracer-ics2019 <tracer@njuics.org>
2019-12-17 20:52:47 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  20:52:47 up  4:21,  1 user,  load average: 0.64, 0.48, 0.31 34ff37ff32d7ae1743f7320a5ba8badf5bc8628a

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -2,6 +2,7 @@
 #define __X86_REG_H__
 
 #include "common.h"
+#include "isa/mmu.h"
 
 #define PC_START IMAGE_START
 
@@ -56,6 +57,9 @@ typedef struct {
     uint16_t limit;
     uint32_t base;
   } idtr;
+
+  CR0 cr0;
+  CR3 cr3;
 
 } CPU_state;
 
