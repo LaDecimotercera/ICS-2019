4b000f6c4b31cdc2f3bf06889bf8cad9e1b83b61
tracer-ics2019 <tracer@njuics.org>
2019-12-18 19:01:29 +0800

    >  compile 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  19:01:29 up 30 min,  1 user,  load average: 0.84, 0.78, 0.64 44a24f88a0d49acfa8ea5d82b36c985e47905050

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -75,30 +75,14 @@ uint32_t isa_vaddr_read(vaddr_t addr, int len) {
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  /*if (cpu.cr0.paging == 1) {
+  if (cpu.cr0.paging == 1) {
     if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)) {
       assert(0);
     } 
     else {
       paddr_t paddr = page_translate(addr);
-      return paddr_write(paddr, data, len);
-    }
-  }
-  return paddr_write(addr, data, len);*/
-    if (cpu.cr0.paging) { // 开启分页
-    if (PTE_ADDR(addr) != PTE_ADDR(addr + len - 1)) { // 数据跨页
-      uint8_t byte[4];
-      if (len == 2)
-        *(uint16_t *)byte = data;
-      else
-        *(uint32_t *)byte = data;
-      for (int i = 0; i < len; i++)
-        isa_vaddr_write(addr + i, byte[i], 1);
-    } else {  // 数据没有跨页
-      paddr_t paddr = page_translate(addr);
       paddr_write(paddr, data, len);
     }
-  } else {  // 不分页
-    paddr_write(addr, data, len);
-  } 
+  }
+  else paddr_write(addr, data, len); 
 }
