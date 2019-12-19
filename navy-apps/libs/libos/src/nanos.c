b6e9ae077e77ed9b865875b5bd2be69f84393e2c
tracer-ics2019 <tracer@njuics.org>
2019-12-19 09:18:19 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  09:18:19 up  2:41,  1 user,  load average: 1.33, 0.97, 0.75 89546202766d6a1e6b207049b4dbcd5e98c6e1a0

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -76,7 +76,7 @@ void *_sbrk(intptr_t increment) {
   } 
   else return -1;*/
   extern uint32_t _end;
-  static uint32_t program_break = &_end;
+  static uint32_t program_break = 0;
   if (program_break == 0) {
     program_break = &_end;
     _syscall_(SYS_brk, program_break, 0, 0);  // 第一次调用
