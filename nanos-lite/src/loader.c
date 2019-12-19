ff19b447457aa7238481b83758229980e1ca66eb
tracer-ics2019 <tracer@njuics.org>
2019-12-19 10:09:52 +0800

    >  run 181220031 liweikang Linux debian 4.19.0-6-amd64 #1 SMP Debian 4.19.67-2+deb10u1 (2019-09-20) x86_64 GNU/Linux  10:09:52 up  3:32,  1 user,  load average: 0.51, 0.53, 0.61 95c5eb9ec027704efbe7e7638ac61ea6b93fa1e8

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -76,6 +76,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   
   return elf_ehdr.e_entry;
 }
+
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
